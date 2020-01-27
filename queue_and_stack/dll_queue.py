dfe13a36ff52cec1d65ca03e76f7d8cafeb1254f
Jenn Soderborg <jsoderborg7@gmail.com>
2020-01-27 16:53:57 -0700

    queue is done, had to copy dll into the queue and stack folder for import to work

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,20 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # DLL lets us find the head and tail easily, and the FIFO approach is simple to understand
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # To queue up, we add the item to the last item of the linked list, and increase its link by 1
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # first we have to make sure the queue isn't empty
+        if self.size > 0:
+        # Then we reduce the size and take out the item from the head, and return the value stored in that item
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
